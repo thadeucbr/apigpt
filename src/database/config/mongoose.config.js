a2fa25a9c5695edcf2ea4b28e3727d3ebec418ea
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-09-28 16:02:48 -0300

    Add error handling and process exit in mongoose configuration

--- src/database/config/mongoose.config.js
+++ src/database/config/mongoose.config.js
@@ -6,5 +6,6 @@ export default startMongoose = async () => {
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB: ', error);
+    process.exit(1);
   }
 }
