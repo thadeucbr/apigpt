680b9631c8fc01395f632dc99331949c3d0abc98
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-09-28 16:01:02 -0300

    Add mongoose configuration for connecting to MongoDB

---
+++ src/database/config/mongoose.config.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export default startMongoose = async () => { 
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB: ', error);
+  }
+}
