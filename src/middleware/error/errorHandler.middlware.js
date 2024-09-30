4e93b4f838c1feed754d70e3acd20ffea7c892c5
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-09-30 13:53:36 +0000

    Refactor errorHandler middleware to remove unused parameter

--- src/middleware/error/errorHandler.middlware.js
+++ src/middleware/error/errorHandler.middlware.js
@@ -1,4 +1,4 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, _req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ status: 'error', message: 'Something went wrong!' });
 };
