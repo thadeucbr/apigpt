d3fb643499e7c192897f0917b39f80eec55cab71
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-09-30 13:48:52 +0000

    Refactor service/index.js to remove unused import and commented out code

--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,7 @@
 import getSchemas from './schema/getSchemas.service.js';
 import aiRepository from '../database/repository/ai.repository.js';
-import mongoRepository from '../database/repository/mongo.repository.js';
 
 export default {
   get_schemas: getSchemas,
   ai_repository: aiRepository,
-  // mongo_repository: mongoRepository
 }
