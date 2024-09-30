ab21eb48b7776a70b8f71e810942873878055106
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-09-30 20:30:45 +0000

    Refactor code formatting configuration files and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,21 @@ export default [
     rules: {
       'no-unused-vars': ['warn', { varsIgnorePattern: '^_' }], // Ignorar variáveis que começam com '_'
       'no-undef': 'off', // Desativar a regra 'no-undef'
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          semi: true,
+          singleQuote: true,
+          printWidth: 80,
+          tabWidth: 2,
+          useTabs: false,
+          bracketSpacing: true,
+          trailingComma: 'es5',
+          arrowParens: 'avoid',
+        },
+      ],
     },
+    plugins: ['prettier'],
   },
 ];
