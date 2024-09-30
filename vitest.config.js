309908afcbca6b06da979b1957105921de7e39d4
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-09-30 13:18:16 +0000

    Refactor vitest.config.js to exclude mongoose.config.js from test files

--- vitest.config.js
+++ vitest.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs}'], // Inclua seus arquivos de teste
-    exclude: ['node_modules', 'dist', 'cypress'],
+    exclude: [
+      'node_modules', 
+      'dist', 
+      'cypress', 
+      'src/database/config/mongoose.config.js'
+    ],
     // Adicione as opções desejadas aqui
     silent: false, // Para mostrar logs detalhados
   },
